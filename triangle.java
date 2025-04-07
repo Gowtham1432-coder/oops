900609877349bf418b35ffd0e3e5d0ab1bd0e287
Gowtham1432 <ayodhyapuramgowthamreddy@gmail.com>
2025-04-07 11:54:21 +0530

    Create triangle.java

---
+++ triangle.java
@@ -0,0 +1,10 @@
+public class triangle {
+
+    public static void main(String[] args) {
+        int rows = 5;  
+        for (int i = 0; i < rows; i++) { 
+            for (int j = 0; j <i+1; j++) {
+                System.out.print("*"); 
+            }
+            System.out.println();
+        }}}
